8f7924f9dcf79a331fa0a1f6a98a6312f74b76d1
Samad Amir <samadamir2003@gmail.com>
2025-08-13 23:37:28 +0530

    Initial commit

---
+++ mydb.py
@@ -0,0 +1,19 @@
+import mysql.connector;
+
+dataBase=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='Samadamir@7867'
+)
+
+# cursor object
+
+cursorObject=dataBase.cursor()
+
+#create DB
+
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("All Done  !!") # only for testing that the code is working fine
+
+
