49de649dd486695151b70d96efbb4c05aa6ad2f2
Samad Amir <141720399+Samaa21@users.noreply.github.com>
2025-08-18 11:55:39 +0530

    Update mydb.py

--- mydb.py
+++ mydb.py
@@ -1,19 +1,18 @@
-import mysql.connector;
+import mysql.connector
+import os
+from dotenv import load_dotenv
 
-dataBase=mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='85d70dde01e2e33496c0f186cd77a6ee0b34423b3c26a74fda8eecdb21d091e4'
+# Load .env variables
+load_dotenv()
+
+dataBase = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
 )
 
-# cursor object
-
-cursorObject=dataBase.cursor()
-
-#create DB
-
+cursorObject = dataBase.cursor()
 cursorObject.execute("CREATE DATABASE elderco")
-
-print("All Done  !!") # only for testing that the code is working fine
+print("All Done !!")
 
 
